b234aa2d50e29b09525e2bdd752fed087a481dc7
Josh Forner <fornerjos@gmail.com>
2020-05-26 08:40:42 -0400

    First Commit

---
+++ ShoppingCartLab/Controllers/MovieController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShoppingCartLab.Models;
+
+namespace ShoppingCartLab.Controllers
+{
+    public class MovieController : Controller
+    {
+        public List<Movie> movies = new List<Movie>()
+        {
+            new Movie("Superbad", 120),
+            new Movie("Zombieland", 140),
+            new Movie("Avatar", 200)
+        };
+
+        public List<Movie> cart = new List<Movie>();
+            
+        public IActionResult Index()
+        {
+            string movielistjson = JsonSerializer.Serialize(movies);
+            HttpContext.Session.SetString("Movies", movielistjson);
+            string cartjson = JsonSerializer.Serialize(cart);
+            HttpContext.Session.SetString("Cart", cartjson);
+            return RedirectToAction("MovieList");
+        }
+
+        public IActionResult Movielist(Movie a)
+        {
+            return View(a);
+        }
+
+        public IActionResult Cart(Movie a) 
+        {
+            string movie = JsonSerializer.Serialize(a);
+            HttpContext.Session.SetString("Movie", movie);
+            return RedirectToAction("AddToCart");
+        }
+
+        public IActionResult AddToCart(Movie a)
+        {
+            string cartlist = HttpContext.Session.GetString("Cart");
+            List<Movie> cart = JsonSerializer.Deserialize<List<Movie>>(cartlist);
+            
+            if (cart.Count == 0)
+            {
+                cart.Add(a);
+                cartlist = JsonSerializer.Serialize(cart);
+                HttpContext.Session.SetString("Cart", cartlist);
+            }
+            else
+            {
+                
+               
+                    if (cart.Any(x => x.Title == a.Title))
+                    {
+                        return View("ErrorPage");
+                    }            
+                    
+                    
+              
+                        cart.Add(a);
+                        cartlist = JsonSerializer.Serialize(cart);
+                        HttpContext.Session.SetString("Cart", cartlist);
+            }
+            return RedirectToAction("MovieList");
+        }
+
+        public IActionResult CartList()
+        {
+            return View();
+        }
+
+        public IActionResult Receipt()
+        {
+            
+            return View();
+        }
+
+    }
+}
---
+++ ShoppingCartLab/Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCartLab.Models
+{
+    public class Movie
+    {
+        public int Runtime { get; set; }
+        public string Title { get; set; }
+
+
+        public Movie() { }
+
+        public Movie(string Title, int Runtime)
+        {
+            this.Title = Title;
+            this.Runtime = Runtime;
+        }
+    }
+}
